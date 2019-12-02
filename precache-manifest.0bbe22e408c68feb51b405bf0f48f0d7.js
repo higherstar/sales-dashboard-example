ecdb3bd96c1663e7211f3fc82ee3a4079d89647a
Gold Byol <goldbyol@outlook.com>
2019-12-02 16:56:32 +0100

    Updates

---
+++ precache-manifest.0bbe22e408c68feb51b405bf0f48f0d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a953e6bbf1fe30fdabe5b8b49033953",
+    "url": "/sales-dashboard-example/index.html"
+  },
+  {
+    "revision": "886e0d6f33f145eb13fc",
+    "url": "/sales-dashboard-example/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5692849238f43a2241c9",
+    "url": "/sales-dashboard-example/static/js/2.9b476d28.chunk.js"
+  },
+  {
+    "revision": "886e0d6f33f145eb13fc",
+    "url": "/sales-dashboard-example/static/js/main.526067eb.chunk.js"
+  },
+  {
+    "revision": "ccb5e0702506093eddfd",
+    "url": "/sales-dashboard-example/static/js/runtime-main.5d834773.js"
+  }
+]);
